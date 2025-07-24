6a8def2c692164cab19c277168264b407f589706
Vishnu Deep PandurangaRao <143198224+vdspw@users.noreply.github.com>
2025-07-23 21:43:12 -0700

    Create Simple_design_tb.sv
    Assertions to find and verify the DUT -- catches the Mutually asserted A and B signals.

---
+++ Simple_design_tb.sv
@@ -0,0 +1,37 @@
+
+module simple_design_tb ;
+  //testbench signals
+  logic clk;
+  logic  a;
+  logic  b;
+  
+  //instantitation
+  simple_design uut (.clk(clk), .a(a), .b(b));
+  
+  // clocking block
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+  
+  // assertion
+  always@(posedge clk) begin 
+  if ( a & b )
+    begin
+      $display("ERROR : Mutually asserted check failed at time : 0t ", $time);
+    end
+  end
+  
+  //stimulus
+  initial begin
+    #100;
+    $finish;
+  end
+    
+    //monitor block
+   initial begin 
+      $monitor ( " TIME : %0t ---- a = %b ----- b = %b", $time , a,b);
+    end
+  
+  
+endmodule
